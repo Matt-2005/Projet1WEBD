034a765d6c8433f438c2d602cce8f8484dbf385e
Matt-2005 <76447374+Matt-2005@users.noreply.github.com>
2024-02-06 14:16:26 +0100

    detail OK

--- detail.js
+++ detail.js
@@ -25,29 +25,35 @@ function movieDetails() {
             `;
             divDetail.appendChild(newDiv);
           })
-            fetch(`https://api.themoviedb.org/3/movie/${movieId}/reviews`, options)
-                .then(response => response.json())
-                    .then(overview => {
-                        for (let i = 0; i < overview.results.length; i++) {
-                            let divComments = document.querySelector("#reviews");
-                            let commentDiv = document.createElement("div");
-                            commentDiv.className = "comment";
-                                if (overview.results[i].author_details.avatar_path == null) {
-                                    avatar = "https://www.terre-net.fr/medias/farmer/actualites/2018/09/26/224013/illustration-1.jpg";
-                                } else{
-                                      avatar = overview.results[i].author_details.avatar_path;
-                                    }
-                              
-                            }
-                            avatar = overview.results[i].author_details.avatar_path;
-                            console.log(avatar);
-                            commentDiv.innerHTML = `
-                                <h3>${overview.results[i].author}</h3>
-                                <img src = "https://image.tmdb.org/t/p/w200/${avatar}"></img>
-                                <p>${overview.results[i].content}</p>
-                                `;
-                              divComments.appendChild(commentDiv);
-                  })
+          fetch(`https://api.themoviedb.org/3/movie/${movieId}/reviews`, options)
+          .then(response => response.json())
+          .then(overview => {
+              for (let i = 0; i < overview.results.length; i++) {
+                  let divComments = document.querySelector("#reviews");
+                  let commentDiv = document.createElement("div");
+                  commentDiv.className = "comment";
+                  let avatarPath = overview.results[i].author_details.avatar_path;
+                  let avatar;
+                  if (avatarPath === null) {
+                      // Utilisez l'URL de l'image par défaut si avatar_path est null, ou ne commence pas par '/http'
+                      avatar = "https://www.logiquetechno.com/wp-content/uploads/2020/11/retirer-photo-de-profil-facebook.png";
+                  } else {
+                      // Vérifiez si le chemin commence par un slash, ce qui indique qu'il s'agit d'un chemin relatif à l'API
+                      if (avatarPath.startsWith('/')) {
+                          avatar = `https://image.tmdb.org/t/p/w200${avatarPath}`;
+                      } else {
+                          // Si le chemin d'accès contient déjà une URL complète (commence par 'http'), utilisez-le tel quel
+                          avatar = avatarPath;
+                      }
+                  }
+                  commentDiv.innerHTML = `
+                      <h3>${overview.results[i].author}</h3>
+                      <img src="${avatar}" alt="Avatar" style="width: 60px; height: 40px;"></img>
+                      <p>${overview.results[i].content}</p>
+                      `;
+                  divComments.appendChild(commentDiv);
+              }
+          })      
                   .catch(err => console.error(err));
 
 }
