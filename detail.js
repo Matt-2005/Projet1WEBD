3180ee3f3ba3664817f33b03305c15c5de3adba9
Matt-2005 <76447374+Matt-2005@users.noreply.github.com>
2024-02-06 13:08:40 +0100

    id

--- detail.js
+++ detail.js
@@ -6,15 +6,25 @@ function movieDetails() {
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYmU1MzlmYTNmYzc3MDI0NTRjZTNjZDFhYWIwMjc2YSIsInN1YiI6IjY1YjIxZDI5YWIxYmM3MDE0YmE4NjE4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W8x7wvMrW0o9YIM5rloKsmvWRRXtnPYfIT_NASDh0lQ'
         }
       };
-      
-      fetch('https://api.themoviedb.org/3/movie/movie_id?language=en-US', options)
-        .then(response => response.json())
-        .then(response => {
-            let divDetail = document.querySelector("#detail");
-            
-        })
-        .catch(err => console.error(err));
+
+    const url = window.location.search;
+    const urlParameters = new URLSearchParams(url);
+    const movieID = urlParameters.get('id');
+    console.log(movieID);
+
+    fetch(`https://api.themoviedb.org/3/movie/${movieID}`, options)
+    .then(response => response.json())
+    .then(response => {
+        let divDetail = document.querySelector("#detail");
+        newDiv = document.createElement("div");
+        newDiv.innerHTML = `
+          <img src = "https://image.tmdb.org/t/p/w200/${response.poster_path}">
+        
+        `;
+        divDetail.appendChild(newDiv);
+    })
+    .catch(err => console.error(err));
 
 }
 
-movieDetails
+movieDetails()
