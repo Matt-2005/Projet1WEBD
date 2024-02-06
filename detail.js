fad7ff88838a92ee5c5160516e3ff965750c4a18
Matt-2005 <76447374+Matt-2005@users.noreply.github.com>
2024-02-06 13:12:54 +0100

    id

--- detail.js
+++ detail.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-function movieDetails() {
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYmU1MzlmYTNmYzc3MDI0NTRjZTNjZDFhYWIwMjc2YSIsInN1YiI6IjY1YjIxZDI5YWIxYmM3MDE0YmE4NjE4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W8x7wvMrW0o9YIM5rloKsmvWRRXtnPYfIT_NASDh0lQ'
-        }
-      };
-
-    const url = window.location.search;
-    const urlParameters = new URLSearchParams(url);
-    const movieID = urlParameters.get('id');
-    console.log(movieID);
-
-    fetch(`https://api.themoviedb.org/3/movie/${movieID}`, options)
-    .then(response => response.json())
-    .then(response => {
-        let divDetail = document.querySelector("#detail");
-        newDiv = document.createElement("div");
-        newDiv.innerHTML = `
-          <img src = "https://image.tmdb.org/t/p/w200/${response.poster_path}">
-        
-        `;
-        divDetail.appendChild(newDiv);
-    })
-    .catch(err => console.error(err));
-
-}
-
-movieDetails()
-=======
 movieId = 0;
 
 function movieDetails(movieId) {
@@ -59,4 +27,3 @@ function movieDetails(movieId) {
 }
 
 movieDetails(movieId)
->>>>>>> e343b031e77946644cfe11d3d8aacdfa67292629
