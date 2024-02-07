41ffbb1b298a721706c90c4170306e41851bdc20
Midorizaru <thomas.schaller@supinfo.com>
2024-02-07 15:31:23 +0100

    changement searchjs

--- search.js
+++ search.js
@@ -1,5 +1,7 @@
 function movieResearch() {
 
+    const query = document.querySelector("#searchInput").value;
+
     const options = {
         method: 'GET',
         headers: {
@@ -8,17 +10,23 @@ function movieResearch() {
         }
       };
       
-      fetch('https://api.themoviedb.org/3/search/movie?query=django&include_adult=false&language=en-US&page=1', options)
-        .then(response => response.json())
-        .then(response => {
-            let divResearch = document.querySelector("#research");
-            let newDiv = document.createElement("div");
-            newDiv.className = "movie";
-            newDiv.innerHTML = `
-            
-            barre de recherche
-            `
-        })
-        .catch(err => console.error(err));
+      fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
+      .then(response => response.json())
+      .then(response => {
+          let divResearch = document.querySelector("#research");
+          divResearch.innerHTML = "";
+          response.results.forEach(movie => {
+              let newDiv = document.createElement("div");
+              newDiv.className = "movie";
+              newDiv.innerHTML = `
+                <img src = "https://image.tmdb.org/t/p/w200/${movie.poster_path}">
+                <h2>${movie.title}</h2>
+              `;
+              divResearch.appendChild(newDiv);
+          });
+      })
+      .catch(err => console.error(err));
 
 }
+
+movieResearch()
